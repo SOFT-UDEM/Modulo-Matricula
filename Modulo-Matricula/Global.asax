6d98461cd3d45cba3eec8d5fee09bd62a068748e
Carlos Quintero <carlosquintero95nic@gmail.com>
2021-07-01 20:42:45 -0600

    Add project files.

---
+++ Modulo-Matricula/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Modulo_Matricula.MvcApplication" Language="C#" %>
---
+++ Modulo-Matricula/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Modulo_Matricula.Startup))]
+namespace Modulo_Matricula
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
