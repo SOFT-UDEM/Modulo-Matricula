dd959ace80c83d85b25180d9708d589595755852
kirito-780 <jonaguevara780@gmail.coom>
2021-07-11 17:39:40 -0600

    Creacion de Filters, Vista Login y validacion de login

--- Modulo-Matricula/Controllers/AccesoLoginController.cs
+++ Modulo-Matricula/Controllers/AccesoLoginController.cs
@@ -9,9 +9,63 @@ namespace Modulo_Matricula.Controllers
     public class AccesoLoginController : Controller
     {
         // GET: AccesoLogin
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
+
+
+
+        // Metodo post para capturar el User y el Password....
+
+        [HttpPost]
+        public ActionResult Login(string usuario, string pass)
+        {
+            try
+            {
+
+                //abrimos coneccion a la base de datos...
+
+                using (bdsistemaEntities db = new bdsistemaEntities())
+                {
+                    //declaramos la variable que captura los datos del los imput...
+
+                    var oUser = (from d in db.Usuarios
+                                 where d.Usuario == usuario.Trim() && d.Contrasena == pass.Trim()
+                                 select d).FirstOrDefault();
+
+
+                    //validamos la informacion 
+
+                    if (oUser == null)
+                    {
+                        ViewBag.Error = "Usuario o contraseña invalida";
+                        return View();
+                    }
+
+
+                    // cramos el filtro para bloquear el paso a las demas páginas...
+                    Session["User"] = oUser;
+
+                }
+
+                // si todo sale bien retornamos  a la pagina de inicio...
+                return RedirectToAction("Index", "Home");
+            }
+
+
+
+            //en caso de error mandamos un mensaje...
+
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+
+
+
     }
 }
---
+++ Modulo-Matricula/Filters/VerificarSeccion.cs
@@ -0,0 +1,41 @@
+﻿using Modulo_Matricula.Controllers;
+using Modulo_Matricula.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Modulo_Matricula.Filters
+{
+    public class VerificarSeccion : ActionFilterAttribute
+    {
+        private Usuarios oUsuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (Usuarios)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+
+                    if (filterContext.Controller is AccesoLoginController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/AccesoLogin/Login");
+                    }
+
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/AccesoLogin/Login");
+            }
+
+        }
+    }
+}
