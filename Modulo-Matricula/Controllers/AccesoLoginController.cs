27c471025553014a362c01103d0d323b4c18d286
kirito-780 <jonaguevara780@gmail.coom>
2021-07-11 15:44:40 -0600

    actualizacion y LayaoutLogin mas Controlador AccesoLogin

---
+++ Modulo-Matricula/Controllers/AccesoLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Modulo_Matricula.Controllers
+{
+    public class AccesoLoginController : Controller
+    {
+        // GET: AccesoLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
