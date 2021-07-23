d7fc04322039f27f4871ee04aceb6f511d12934d
Kirito-780 <jonaguevara780@gmail.com>
2021-07-22 20:52:47 -0600

    Agrege los DataAnotation de la tabla Matricula y el controler con sus vistas

--- Modulo-Matricula/Matricula.cs
+++ Modulo-Matricula/Matricula.cs
@@ -11,12 +11,46 @@ namespace Modulo_Matricula
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    ///* Agregamos las librerias (using System.ComponentModel;using System.ComponentModel.DataAnnotations;)
+    // para utilizar las dataNotation para validar y utilizar ciertas caracteristicas que nor permitiran
+    // un mejor manejo de los datos que se ingresaran en los formularios.
+    //*/
+
     public partial class Matricula
     {
+        ///*      
+        //   *[Required(ErrorMessage = "Este campo esta Vacío.")] --campo requerido y un mensaje de error.
+        //   *[Compare("Contrasena")] --nos funciona para comparar con otro imput o campo.
+
+        //   *[DataType(DataType.Date)] --para dar formato de fecha al campo.
+
+        //   *[DisplayName(" ")] --cambia el nobre del campo en el formulario.
+
+        //   *[MaxLengthAttribute(length: 80)] --definimos el maximo de caracteres que se pueden ingresar.
+
+        //   *[EmailAddress] --Valida si es un correo electronico.
+
+        //   *[DataType(DataType.Password)] --lo utilizamos para que la contraseña no sea legible (es decir Pass:*****).
+        //   *[StringLength(8, ErrorMessage = ".... ", MinimumLength = 8)] --definimos la cantidad de caracteres  a ingresar y mandamos un mensaje de error. 
+        //   *[RegularExpression(@"^((?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])(?=.*d)).+$")] --utilizamos una expresion regular para validar si hay 1 numero, 1 simbolo y 1 letra en Mayuscula
+        // */
+
         public int IdMatricula { get; set; }
+
+        [DisplayName("Id del Alumno: ")]
+        [Required(ErrorMessage = "Este campo esta Vacío.")]
         public Nullable<int> IdAlumno { get; set; }
+
+        [DisplayName("Fecha de registro:  ")]
+        [Required(ErrorMessage = "Este campo esta Vacío.")]
+        [DataType(DataType.Date)]
         public Nullable<System.DateTime> FechaRegistro { get; set; }
+
+        [DisplayName("Id del Usuario Que lo registro: ")]
+        [Required(ErrorMessage = "Este campo esta Vacío.")]
         public Nullable<int> IdUsuario { get; set; }
     
         public virtual Alumnos Alumnos { get; set; }
