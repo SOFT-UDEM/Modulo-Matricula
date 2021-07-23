a25dfcbc405b0ca762663d231f41156828038b88
Kirito-780 <jonaguevara780@gmail.com>
2021-07-22 18:06:31 -0600

    se agrego con exito las DataAnotation de la tabla Alumnos

--- Modulo-Matricula/Usuarios.cs
+++ Modulo-Matricula/Usuarios.cs
@@ -24,27 +24,27 @@ namespace Modulo_Matricula
         }
 
 
-        /* Agregamos las librerias (using System.ComponentModel;using System.ComponentModel.DataAnnotations;)
-         para utilizar las dataNotation para validar y utilizar ciertas caracteristicas que nor permitiran
-         un mejor manejo de los datos que se ingresaran en los formularios.
-        */
+        ///* Agregamos las librerias (using System.ComponentModel;using System.ComponentModel.DataAnnotations;)
+        // para utilizar las dataNotation para validar y utilizar ciertas caracteristicas que nor permitiran
+        // un mejor manejo de los datos que se ingresaran en los formularios.
+        //*/
 
-        /*      
-           *[Required(ErrorMessage = "Este campo esta Vacío.")] --campo requerido y un mensaje de error.
-           *[Compare("Contrasena")] --nos funciona para comparar con otro imput o campo.
+        ///*      
+        //   *[Required(ErrorMessage = "Este campo esta Vacío.")] --campo requerido y un mensaje de error.
+        //   *[Compare("Contrasena")] --nos funciona para comparar con otro imput o campo.
             
-           *[DataType(DataType.Date)] --para dar formato de fecha al campo.
+        //   *[DataType(DataType.Date)] --para dar formato de fecha al campo.
             
-           *[DisplayName(" ")] --cambia el nobre del campo en el formulario.
+        //   *[DisplayName(" ")] --cambia el nobre del campo en el formulario.
 
-           *[MaxLengthAttribute(length: 80)] --definimos el maximo de caracteres que se pueden ingresar.
+        //   *[MaxLengthAttribute(length: 80)] --definimos el maximo de caracteres que se pueden ingresar.
            
-           *[EmailAddress] --Valida si es un correo electronico.
+        //   *[EmailAddress] --Valida si es un correo electronico.
            
-           *[DataType(DataType.Password)] --lo utilizamos para que la contraseña no sea legible (es decir Pass:*****).
-           *[StringLength(8, ErrorMessage = ".... ", MinimumLength = 8)] --definimos la cantidad de caracteres  a ingresar y mandamos un mensaje de error. 
-           *[RegularExpression(@"^((?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])(?=.*d)).+$")] --utilizamos una expresion regular para validar si hay 1 numero, 1 simbolo y 1 letra en Mayuscula
-         */
+        //   *[DataType(DataType.Password)] --lo utilizamos para que la contraseña no sea legible (es decir Pass:*****).
+        //   *[StringLength(8, ErrorMessage = ".... ", MinimumLength = 8)] --definimos la cantidad de caracteres  a ingresar y mandamos un mensaje de error. 
+        //   *[RegularExpression(@"^((?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])(?=.*d)).+$")] --utilizamos una expresion regular para validar si hay 1 numero, 1 simbolo y 1 letra en Mayuscula
+        // */
 
 
 
@@ -70,18 +70,18 @@ namespace Modulo_Matricula
         [DisplayName("Ingrese su Contraseña: ")]
         [StringLength(maximumLength: 64, MinimumLength = 8, ErrorMessage = "Solo se puede ingresar 8 caracteres")]
         [DataType(DataType.Password)]
-        [RegularExpression(@"^\A([^a-z][^A-Z][^0-9][^(!#$%&¡?¿@)])\Z$", ErrorMessage = "Debe contener Mayusculas,Numeros y Simbolos.")]
+        //[RegularExpression(@"^\A([^a-z][^A-Z][^0-9][^(!#$%&¡?¿@)])\Z$", ErrorMessage = "Debe contener Mayusculas,Numeros y Simbolos.")]
         
         public string Contrasena { get; set; }
 
 
         //agregaos este campo para confirmar contraseña.
 
-        [DisplayName("Confirmar tu Contraseña: ")]
-        [Compare("Contrasena")]
-        [DataType(DataType.Password)]
-        [StringLength(maximumLength: 64, MinimumLength = 8, ErrorMessage = "Solo se puede ingresar 8 caracteres")]
-        public string Contrasena2 { get; set; }
+        //[DisplayName("Confirmar tu Contraseña: ")]
+        //[Compare("Contrasena")]
+        //[DataType(DataType.Password)]
+        //[StringLength(maximumLength: 64, MinimumLength = 8, ErrorMessage = "Solo se puede ingresar 8 caracteres")]
+        //public string Contrasena2 { get; set; }
 
 
         [DisplayName("Rol de administrador:")]
